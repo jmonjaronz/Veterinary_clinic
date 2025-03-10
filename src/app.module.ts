384e4a763454dde1f3538c2f0867a6b18645eadd
jmonjaronz <jmonjaron@gmail.com>
2025-03-10 16:59:16 -0500

    feat(app.module.ts): add all configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PersonsModule } from './persons/persons.module';
+import { SpeciesModule } from './species/species.module';
+import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AppConfigModule,
+    DatabaseModule,
+    AuthModule,
+    UsersModule,
+    PersonsModule,
+    SpeciesModule,
+    PetsModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
