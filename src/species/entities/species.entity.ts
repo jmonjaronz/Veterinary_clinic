8c14d8ebadf1d9b41aa90c27a567932f4674a74b
jmonjaronz <jmonjaron@gmail.com>
2025-03-10 17:14:58 -0500

    feat(species.entity.ts): create entity for species

---
+++ src/species/entities/species.entity.ts
@@ -0,0 +1,23 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, OneToMany } from 'typeorm';
+import { Pet } from '../../pets/entities/pet.entity';
+
+@Entity({ name: 'species' })
+export class Species {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column({ nullable: true })
+    type: string;
+
+    @Column({ type: 'text', nullable: true })
+    description: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    created_at: Date;
+
+    @OneToMany(() => Pet, pet => pet.species)
+    pets: Pet[];
+}
