1e2218c31ccc9b50a51e7374253e05bacfe24af7
jmonjaronz <jmonjaron@gmail.com>
2025-03-10 17:45:01 -0500

    feat(update-user.dto.ts): add validation for update-user

---
+++ src/users/dto/update-user.dto.ts
@@ -0,0 +1,14 @@
+import { IsOptional, IsString, MinLength } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUserDto } from './create-user.dto';
+
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsOptional()
+    @IsString({ message: 'El tipo de usuario debe ser una cadena de texto' })
+    user_type?: string;
+
+    @IsOptional()
+    @IsString({ message: 'La contraseña debe ser una cadena de texto' })
+    @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
+    password?: string;
+}
