9c41130da7dbad832f08d19946a9d96cdf0d2aa2
jmonjaronz <jmonjaron@gmail.com>
2025-03-10 17:32:20 -0500

    feat(create-user.dto.ts): add validation for create user

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,16 @@
+import { IsNotEmpty, IsString, IsNumber, MinLength } from 'class-validator';
+
+export class CreateUserDto {
+    @IsNotEmpty({ message: 'El ID de la persona es requerido' })
+    @IsNumber({}, { message: 'El ID de la persona debe ser un número' })
+    person_id: number;
+
+    @IsNotEmpty({ message: 'El tipo de usuario es requerido' })
+    @IsString({ message: 'El tipo de usuario debe ser una cadena de texto' })
+    user_type: string;
+
+    @IsNotEmpty({ message: 'La contraseña es requerida' })
+    @IsString({ message: 'La contraseña debe ser una cadena de texto' })
+    @MinLength(6, { message: 'La contraseña debe tener al menos 6 caracteres' })
+    password: string;
+}
