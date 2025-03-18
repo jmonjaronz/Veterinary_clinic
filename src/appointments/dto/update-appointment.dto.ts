3f2d702615d0575448106725a33f3264477f481f
jmonjaronz <jmonjaron@gmail.com>
2025-03-18 16:18:00 -0500

    feat(update-appointment.deto.ts): add validation for update

---
+++ src/appointments/dto/update-appointment.dto.ts
@@ -0,0 +1,29 @@
+import { IsOptional, IsNumber, IsDate, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class UpdateAppointmentDto {
+    @IsOptional()
+    @IsNumber({}, { message: 'El ID de la mascota debe ser un número' })
+    pet_id?: number;
+
+    @IsOptional()
+    @IsNumber({}, { message: 'El ID del veterinario debe ser un número' })
+    veterinarian_id?: number;
+
+    @IsOptional()
+    @IsString({ message: 'El tipo de cita debe ser una cadena de texto' })
+    appointment_type?: string;
+
+    @IsOptional()
+    @Type(() => Date)
+    @IsDate({ message: 'La fecha de la cita debe ser una fecha válida' })
+    date?: Date;
+
+    @IsOptional()
+    @IsString({ message: 'El estado debe ser una cadena de texto' })
+    status?: string;
+
+    @IsOptional()
+    @IsString({ message: 'El documento debe ser una cadena de texto' })
+    document?: string;
+}
