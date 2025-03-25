51f6a427653a45708e566ebd0f633fa5e5b36b9a
jmonjaronz <jmonjaron@gmail.com>
2025-03-25 10:48:40 -0500

    feat(main.ts): change for image

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,8 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     transform: true,
-    forbidNonWhitelisted: true,
+    transformOptions: { enableImplicitConversion: true },
+    forbidNonWhitelisted: false,
   }));
 
   // Habilitar CORS
