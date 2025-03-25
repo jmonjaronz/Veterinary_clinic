7b1144c05f8ca3182b3b329465a64529b00c9109
jmonjaronz <jmonjaron@gmail.com>
2025-03-25 10:51:00 -0500

    fix(upload-pet-image.dto.ts): add transform

--- src/pets/dto/upload-pet-image.dto.ts
+++ src/pets/dto/upload-pet-image.dto.ts
@@ -1,5 +1,5 @@
-import { IsOptional, IsNumber, Transform } from 'class-validator';
-import { Type } from 'class-transformer';
+import { IsOptional, IsNumber } from 'class-validator';
+import { Type, Transform } from 'class-transformer';
 
 export class UploadPetImageDto {
     @IsNumber()
