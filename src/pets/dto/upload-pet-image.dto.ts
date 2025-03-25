144964d0e4ef5a122aeb5f2bd7d76c3f799c20c9
jmonjaronz <jmonjaron@gmail.com>
2025-03-25 10:43:35 -0500

    feat(upload-pet-image.dto.ts): add more validation

--- src/pets/dto/upload-pet-image.dto.ts
+++ src/pets/dto/upload-pet-image.dto.ts
@@ -1,4 +1,16 @@
+import { IsOptional, IsNumber, Transform } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class UploadPetImageDto {
+    @IsNumber()
+    @Type(() => Number)
     petId: number;
+
+    @IsOptional()
+    @Transform(({ value }) => {
+        if (value === 'true') return true;
+        if (value === 'false') return false;
+        return Boolean(value);
+    })
     isMain?: boolean;
 }
