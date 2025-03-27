8aa648d2f78e59743432e8a276041e57f1ccaae6
jmonjaronz <jmonjaron@gmail.com>
2025-03-26 19:26:42 -0500

    feat(hospitalizations.controller.ts): add filters

--- src/hospitalizations/hospitalizations.controller.ts
+++ src/hospitalizations/hospitalizations.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Query } f
 import { HospitalizationsService } from './hospitalizations.service';
 import { CreateHospitalizationDto } from './dto/create-hospitalization.dto';
 import { UpdateHospitalizationDto } from './dto/update-hospitalization.dto';
+import { HospitalizationFilterDto } from './dto/hospitalization-filter.dto';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
 @Controller('hospitalizations')
@@ -17,27 +18,28 @@ export class HospitalizationsController {
     @UseGuards(JwtAuthGuard)
     @Get()
     findAll(
+        @Query() filterDto: HospitalizationFilterDto,
         @Query('pet_id') petId?: string,
         @Query('veterinarian_id') veterinarianId?: string,
         @Query('status') status?: string
     ) {
         if (petId) {
-        return this.hospitalizationsService.findByPet(+petId);
+            return this.hospitalizationsService.findByPet(+petId, filterDto);
         }
         
         if (veterinarianId) {
-        return this.hospitalizationsService.findByVeterinarian(+veterinarianId);
+            return this.hospitalizationsService.findByVeterinarian(+veterinarianId, filterDto);
         }
         
         if (status === 'active') {
-        return this.hospitalizationsService.findActive();
+            return this.hospitalizationsService.findActive(filterDto);
         }
         
         if (status === 'discharged') {
-        return this.hospitalizationsService.findDischarged();
+            return this.hospitalizationsService.findDischarged(filterDto);
         }
         
-        return this.hospitalizationsService.findAll();
+        return this.hospitalizationsService.findAll(filterDto);
     }
 
     @UseGuards(JwtAuthGuard)
