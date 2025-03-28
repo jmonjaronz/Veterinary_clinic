e6b4d70bbc07d93792426fb3776e2e82976ec6e7
jmonjaronz <jmonjaron@gmail.com>
2025-03-27 19:25:19 -0500

    feat(persons.module.ts): update for use filter by DNI

--- src/persons/persons.module.ts
+++ src/persons/persons.module.ts
@@ -1,13 +1,21 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { HttpModule } from '@nestjs/axios';
 import { PersonsService } from './persons.service';
 import { PersonsController } from './persons.controller';
 import { Person } from './entities/person.entity';
+import { DniSearchService } from './dni-search.service';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Person])],
+    imports: [
+        TypeOrmModule.forFeature([Person]),
+        HttpModule.register({
+            timeout: 5000,
+            maxRedirects: 5,
+        }),
+    ],
     controllers: [PersonsController],
-    providers: [PersonsService],
+    providers: [PersonsService, DniSearchService],
     exports: [PersonsService],
 })
 export class PersonsModule {}
