d38e9abb42ac5629c6f0a3407ae94e4c9f1f5fe8
jmonjaronz <jmonjaron@gmail.com>
2025-04-12 14:44:08 -0500

    feat(vaccination-plns.module.ts): update with use for vaccine

--- src/vaccination-plans/vaccination-plans.module.ts
+++ src/vaccination-plans/vaccination-plans.module.ts
@@ -2,13 +2,22 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { VaccinationPlansService } from './vaccination-plans.service';
 import { VaccinationPlansController } from './vaccination-plans.controller';
+import { VaccinationRecordsController } from './vaccination-records.controller';
 import { VaccinationPlan } from './entities/vaccination-plan.entity';
+import { VaccinationRecord } from './entities/vaccination-record.entity';
 import { Pet } from '../pets/entities/pet.entity';
 import { SpeciesVaccinationPlan } from '../species-vaccination-plans/entities/species-vaccination-plan.entity';
+import { Vaccine } from '../vaccines/entities/vaccine.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([VaccinationPlan, Pet, SpeciesVaccinationPlan])],
-    controllers: [VaccinationPlansController],
+    imports: [TypeOrmModule.forFeature([
+        VaccinationPlan, 
+        VaccinationRecord,
+        Pet, 
+        SpeciesVaccinationPlan,
+        Vaccine
+    ])],
+    controllers: [VaccinationPlansController, VaccinationRecordsController],
     providers: [VaccinationPlansService],
     exports: [VaccinationPlansService],
 })
