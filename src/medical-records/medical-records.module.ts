922ae839b8df13370a6fc6b44f114285fdf79aad
jmonjaronz <jmonjaron@gmail.com>
2025-04-22 04:03:03 -0500

    feat(medical-records.module.ts): update module for new structure

--- src/medical-records/medical-records.module.ts
+++ src/medical-records/medical-records.module.ts
@@ -6,9 +6,20 @@ import { MedicalRecord } from './entities/medical-record.entity';
 import { Pet } from '../pets/entities/pet.entity';
 import { Person } from '../persons/entities/person.entity';
 import { Appointment } from '../appointments/entities/appointment.entity';
+import { Treatment } from '../treatments/entities/treatment.entity';
+import { Hospitalization } from '../hospitalizations/entities/hospitalization.entity';
+import { VaccinationRecord } from '../vaccination-plans/entities/vaccination-record.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([MedicalRecord, Pet, Person, Appointment])],
+    imports: [TypeOrmModule.forFeature([
+        MedicalRecord, 
+        Pet, 
+        Person, 
+        Appointment,
+        Treatment,
+        Hospitalization,
+        VaccinationRecord
+    ])],
     controllers: [MedicalRecordsController],
     providers: [MedicalRecordsService],
     exports: [MedicalRecordsService],
