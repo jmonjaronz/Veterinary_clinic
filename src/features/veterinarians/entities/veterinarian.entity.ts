c132a8f01362a5b2d93c18fbca18743c3f2bea81
Luis Bonilla <luis.labp06@gmail.com>
2026-01-16 12:14:26 -0500

    refactor: add relationships and properties for appointments, medical records, surgical consents, and hospitalizations in veterinarian entity

--- src/features/veterinarians/entities/veterinarian.entity.ts
+++ src/features/veterinarians/entities/veterinarian.entity.ts
@@ -1,6 +1,10 @@
+import { Appointment } from "src/features/appointments/entities/appointment.entity";
 import { Company } from "src/features/companies/entities/company.entity";
+import { Hospitalization } from "src/features/hospitalizations/entities/hospitalization.entity";
+import { MedicalRecord } from "src/features/medical-records/entities/medical-record.entity";
 import { Person } from "src/features/persons/entities/person.entity";
-import { CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { SurgicalConsent } from "src/features/surgical-consents/entities/surgical-consent.entity";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({name: 'veterinarians'})
 export class Veterinarian {
@@ -17,6 +21,21 @@ export class Veterinarian {
     @ManyToOne(() => Company)
     @JoinColumn({name: 'company_id'})
     company: Company;
+
+    @Column({name: 'licence_number', type: 'varchar', length: 100, nullable: true})
+    licenceNumber: string | null;
+
+    @OneToMany(() => Appointment, (appointment) => appointment.veterinarian)
+    appointments: Appointment[];
+
+    @OneToMany(() => MedicalRecord, (medicalRecord) => medicalRecord.veterinarian)
+    medicalRecords: MedicalRecord[];
+
+    @OneToMany(() => SurgicalConsent, (surgicalConsent) => surgicalConsent.veterinarian)
+    surgicalConsents: SurgicalConsent[];
+
+    @OneToMany(() => Hospitalization, (hospitalization) => hospitalization.veterinarian)
+    hospitalizations: Hospitalization[]
 
     @CreateDateColumn({ name: 'created_at' })
     createdAt: Date | null;
